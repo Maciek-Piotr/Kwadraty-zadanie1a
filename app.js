e270d1678471d75a7cd04528d75e661ae83de529
Maciek-Piotr <maciej.piotr.piekutowski@gmail.com>
2024-02-06 22:08:50 +0100

    ready

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const btn = document.querySelector("#the_only_button");
-const newDiv = document.createElement("div");
 let txt = 0;
-
-const squareMaker = () => {
+const divMaker = () => {
   console.log("pp");
+  const newDiv = document.createElement("div");
   document.body.appendChild(newDiv);
-
   txt++;
+  newDiv.textContent = txt;
   if (txt % 5 === 0) {
-    newDiv.classList.remove("square");
     newDiv.classList.add("circle");
-  } else {
-    newDiv.classList.add("square");
-    newDiv.classList.remove("circle");
   }
 };
-btn.addEventListener("click", squareMaker);
+btn.addEventListener("click", divMaker);
